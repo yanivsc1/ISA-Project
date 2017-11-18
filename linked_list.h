e790e811f0a06e2914fd3a52fa2a77713dc4a32f
carmellevin <carmellevin@mail.tau.ac.il>
2017-11-18 20:02:27 +0200

    hello

---
+++ linked_list.h
@@ -0,0 +1,31 @@
+#ifndef ISA_LINKED_LIST_H
+#define ISA_LINKED_LIST_H
+
+#include <stdlib.h>
+#include <string.h>
+#include "linked_list.h"
+
+typedef struct node {
+    struct node* prev;
+    struct node* next;
+    char* key;
+    unsigned int value;
+} Node ;
+
+typedef struct linked_list {
+    Node* head;
+    size_t size;
+} List;
+
+
+List* create_list();
+
+Node* create_node(char* key, unsigned int value);
+
+int find(List* list, char* key);
+
+void insert_node(List* list, char* key, unsigned int value);
+
+void free_list(List* list);
+
+#endif //ISA_LINKED_LIST_H
